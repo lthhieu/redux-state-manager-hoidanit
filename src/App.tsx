35cad07045691db7743c51866d7ad70db6522ee5
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-08-22 21:04:23 +0700

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { increment, decrement } from './redux/counter/counterSlice'
 import { useAppSelector, useAppDispatch } from './redux/hooks'
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const count = useAppSelector((state) => state.counter.value)
@@ -18,6 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Button variant='success'>Test Bootstrap</Button>
       <h1>My counter is {count}</h1>
       <button onClick={() => dispatch(increment())}>Increase +1</button>
       <button onClick={() => dispatch(decrement())}>Decrease -1</button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { store } from './redux/store.ts'
 import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
